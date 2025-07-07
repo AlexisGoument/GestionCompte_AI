9c91debf29e23e5860539f2ccddc6fb6ff9aab93
AlexisGoument <alexis.nguyen.goument@gmail.com>
2025-07-07 17:06:59 +0200

    refacto

--- GestionCompteTests/CompteParserTests.cs
+++ GestionCompteTests/CompteParserTests.cs
@@ -7,7 +7,7 @@ namespace GestionCompteTests
     public class CompteParserTests
     {
         [Test]
-        public void Parser_RetourneDonneesCorrectes_PourCsvMinimal()
+        public void Parser_RetourneDonneesCorrectes_FichierMinimal()
         {
             var contenu =
                 """
@@ -18,18 +18,20 @@ namespace GestionCompteTests
                 06/10/2022;-504.61;EUR;Loisir
                 """;
             var parser = new CompteParser();
+
             var donnees = parser.Parser(contenu);
+
             Assert.That(donnees.BalanceReference, Is.EqualTo(8300.00m));
             Assert.That(donnees.DateBalanceReference, Is.EqualTo(new DateOnly(2023, 2, 28)));
             Assert.That(donnees.TauxDeChange, Is.Not.Null);
             Assert.That(donnees.TauxDeChange.JPY, Is.EqualTo(0.482m));
             Assert.That(donnees.TauxDeChange.USD, Is.EqualTo(1.445m));
             Assert.That(donnees.Transactions, Has.Count.EqualTo(1));
-            var t = donnees.Transactions[0];
-            Assert.That(t.Date, Is.EqualTo(new DateOnly(2022, 10, 6)));
-            Assert.That(t.Montant, Is.EqualTo(-504.61m));
-            Assert.That(t.Devise, Is.EqualTo("EUR"));
-            Assert.That(t.Categorie, Is.EqualTo("Loisir"));
+            var transaction = donnees.Transactions[0];
+            Assert.That(transaction.Date, Is.EqualTo(new DateOnly(2022, 10, 6)));
+            Assert.That(transaction.Montant, Is.EqualTo(-504.61m));
+            Assert.That(transaction.Devise, Is.EqualTo("EUR"));
+            Assert.That(transaction.Categorie, Is.EqualTo("Loisir"));
         }
     }
 } 
